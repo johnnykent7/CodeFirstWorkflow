a0b5bd2404ce15bb23313e11fd57393e241ed5e6
Ion <ionglavan@mail.ru>
2018-07-10 21:15:07 +0300

    am creat clasele de baza

---
+++ CodeFirstWorkflow/Video.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CodeFirstWorkflow
+{
+    public class Video
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
